e815f47a2b0d3f5334bbb922bd0a023344ef7030
Arturo <manolo@manolo.es>
2014-05-23 10:50:02 +0200

    La clase Item tiene ahora puede ser dañ/no/beneficioso y en un grado determinado

--- Item.java
+++ Item.java
@@ -10,16 +10,27 @@ public class Item
     private int weight;
     //indica si el item puede ser llevado o no
     private boolean taken;
+    //indica si el item es dañino o no
+    private boolean harmful;
+    //indica el grado en el que el elemento es dañino o beneficioso
+    private int affection;
     
 
     /**
      * Constructor for objects of class Item
      */
-    public Item(String itemName, int weight, boolean taken)
+    public Item(String itemName, int weight, boolean taken, boolean harmful)
     {
         this.itemName = itemName;
         this.weight = weight;
         this.taken = taken;
+        this.harmful=harmful;
+        if(harmful){
+            affection = 2;
+        }else{
+            affection = 1;
+        }
+        
     }
 
     /**
@@ -41,5 +52,13 @@ public class Item
     public boolean getTaken(){
         return taken;
     }
+    
+    /**
+     * Returns 'true¡ if the item is harmful
+     */
+    public boolean getHarmmful(){
+        return harmful;
+    }
+     
 }   
 
